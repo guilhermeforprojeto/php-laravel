4b9e292549990dcfbe97d01918e19d90217d0bbf
guilhermeforprojeto <guylhermesantos@hotmail.com>
2022-10-31 17:34:32 -0300

    Rota para saber mais

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
         <p class="card-date">30/10/2022</p>
         <h5 class="card-title">{{ $event->title }}</h5>
         <p class="card-participants">x participantes</p>
-        <a href="/events/{{ $event->id}}" class="btn btn-primary"> Saber mais</a>
+        <a href="/events/{{ $event->id }}" class="btn btn-primary"> Saber mais</a>
       </div>
     </div>
     @endforeach
