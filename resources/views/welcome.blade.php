c223a5abd300c82e64a102dea17fa992d5d3735b
guilhermeforprojeto <guylhermesantos@hotmail.com>
2022-11-03 22:43:50 -0300

    Logica para a pagina se comportar de forma mais amigavel para interface do usuario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,16 @@
 <div id="search-container" class="col-md-12">
 
   <h1>Busque um Evento</h1>
-  <form action="">
+  <form action="/" method="GET">
     <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
   </form>
 </div>
 <div id="events-container" class="col-md-12">
+  @if($search)
+  <h2>Buscando por {{ $search }}</h2>
+  @else
   <h2>Proximos Eventos</h2>
+  @endif
   <p class="subtitle">Veja os eventos dos proximos dias</p>
   <div id="cards-container" class="row">
     @foreach($events as $event)
@@ -27,8 +31,11 @@
       </div>
     </div>
     @endforeach
-    @if(count($events) == 0)
+    @if(count($events) == 0 && $search)
+    <p>Não foi possível encontrar nenhum evento com {{ $search }}! <a href="/">Ver todos</a></p>
+    @else(count($events) == 0)
     <p>Não há eventos disponíveis</p>
+
     @endif
   </div>
 </div>
